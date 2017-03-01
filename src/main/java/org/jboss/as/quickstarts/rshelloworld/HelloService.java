31d73c8a80e65cc71c1647512fb18ecb23a46d1c
psmanningjr <psmanningjr@gmail.com>
2017-03-01 12:58:35 -0500

    add dbname to connection

--- src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloService.java
@@ -56,7 +56,7 @@ public class HelloService {
 
         try {
             StringBuffer postgresUrl = new StringBuffer("jdbc:postgresql://");
-            postgresUrl.append(String.format("%s:%s",System.getenv("POSTGRESQL_SERVICE_HOST"),
+            postgresUrl.append(String.format("%s:%s/sampledb",System.getenv("POSTGRESQL_SERVICE_HOST"),
                     System.getenv(("POSTGRESQL_SERVICE_PORT"))));
 
             connection = DriverManager.getConnection(postgresUrl.toString());
