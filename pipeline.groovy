c98608736c76a9aa43babd630533f4f753137dbd
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-11 17:42:40 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,5 @@
-pipeline {
-    agent { docker 'maven:3.3.3' }
-    stages {
-        stage('build') {
-            steps {
+node('docker 'maven:3.3.3') {
+   stage('build') {
                 sh 'mvn --version'
-            }
-        }
     }
 }
