1ab43c11e935530fbe6076b35210fdae18b9dc7a
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-12 12:36:24 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,13 @@
 node('maven') {
    //docker maven:3.3.3
+   String pomFileLocation = env.BUILD_CONTEXT_DIR ? "${env.BUILD_CONTEXT_DIR}/pom.xml" : "pom.xml"
+
+
    stage('build') {
       //withMaven(maven:'maven339')
       //def mvnTool = tool 'Maven_3_3_3'
 
       //sh "${mvnTool}/bin/mvn --version"
-      sh "mvn clean install -DskipTests=true "
+      sh "mvn clean install -DskipTests=true -f ${pomFileLocation} "
     }
 }
