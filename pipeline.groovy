2971ee72f5690d259c7631423dc16b59ddcdfc39
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-12 10:34:40 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,8 +4,9 @@ node('maven') {
    //docker maven:3.3.3
   // withMaven(maven:'Maven_3_3_9')
    stage('build') {
-      def mvnTool = tool 'Maven_3_3_3'
+      //def mvnTool = tool 'Maven_3_3_3'
 
-      sh "${mvnTool}/bin/mvn --version"
+      //sh "${mvnTool}/bin/mvn --version"
+       sh "mvn --version"
     }
 }
