93a9b9b9f806c7674ff4466f73c7eaffd6bb3a66
psmanningjr <psmanningjr@users.noreply.github.com>
2017-04-12 10:55:47 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,6 +1,6 @@
-node('maven') {
+node('maven339') {
    //docker maven:3.3.3
-   withMaven(maven:'Maven_3_3_9')
+   //withMaven(maven:'Maven_3_3_9')
    stage('build') {
       //def mvnTool = tool 'Maven_3_3_3'
 
